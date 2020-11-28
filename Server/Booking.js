e46d7f6786450ba8a4a60830a0fcfb128a59bbe5
Improbdrive <sam.wakelam@hotmail.com>
2020-11-28 11:39:50 +0000

    class structure set up

---
+++ Server/Booking.js
@@ -0,0 +1,22 @@
+class Booking {
+    constructor(name, number, email, id){
+        this.name = name; 
+        this.number = number;
+        this.email = email;
+        this.id = id; 
+    }
+    getName(){
+
+    }
+    getNumber(){
+
+    }
+    getEmail(){
+
+    }
+    getId(){
+
+    }
+}
+
+module.exports = Booking;
