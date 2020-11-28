18b222f9b53884eb1668d9f85232658bb1ef238a
Corey Samuels <coreyjsamuels@gmail.com>
2020-11-28 11:35:19 +0000

    starting server

---
+++ Server/Server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function () {
+  console.log("Server is listening on Port ", PORT);
+});
