305afa829b57bb1585885da7faabee482f9aa939
Corey Samuels <coreyjsamuels@gmail.com>
2020-11-28 12:13:23 +0000

    adding nodemon

--- Server/Server.js
+++ Server/Server.js
@@ -6,10 +6,6 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../Client", "home.html"));
-});
-
 app.listen(PORT, function () {
   console.log("Server is listening on Port ", PORT);
 });
