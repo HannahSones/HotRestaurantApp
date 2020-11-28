0532bdfb255a172acc520c73c507f99530011fbd
Corey Samuels <coreyjsamuels@gmail.com>
2020-11-28 11:54:09 +0000

    sending Home page

--- Server/Server.js
+++ Server/Server.js
@@ -1,9 +1,14 @@
 var express = require("express");
+var path = require("path");
 var app = express();
 var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../Client", "home.html"));
+});
 
 app.listen(PORT, function () {
   console.log("Server is listening on Port ", PORT);
