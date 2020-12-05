a78dafc0669d53174aff6b70cc734095db43809e
Improbdrive <sam.wakelam@hotmail.com>
2020-12-05 10:55:56 +0000

    adding ajax calls

--- Client/script.js
+++ Client/script.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
 
+  //reserve html
   const contactName = $("#inputName");
   const phoneNumber = $("#inputPhoneNumber");
   const emailAddress = $("#inputEmail");
   const id = $("#uniqueID");
   const reserveForm = $("#reserveForm");
+
+
+
+  // const viewTables = $(".view-table");
+  // viewTables.on("click","button", makeCall);
 
   reserveForm.submit(function (event) {
     event.preventDefault();
@@ -12,14 +18,14 @@ $(document).ready(function(){
     const name = contactName.val();
     const email = emailAddress.val();
     const phone = phoneNumber.val();
-    const uniqueID = id.val();
+    const uniqueId = id.val();
     console.log("Data received");
-    console.log(name, email, phone, uniqueID); 
+    console.log(name, email, phone, uniqueId); 
     const reserve = {
       "name": name,
       "email": email,
       "phone": phone,
-      "uniqueId": uniqueID
+      "id": uniqueId
     };
     console.log("reserve =", reserve);
   
@@ -27,13 +33,18 @@ $(document).ready(function(){
       type: "POST",
       url: "/api/reserve",
       data: reserve,
-      success: () => { console.log('success!'); },
+      success: () => { 
+        console.log('success! The server has received your data.'); 
+
+        // Clear the form when submitting
+        contactName.val("");
+        emailAddress.val("");
+        phoneNumber.val("");
+        id.val("");
+      },
       error: (error) => { console.log('error', error); }
     });
-
   });
 
 
 });
-
-
